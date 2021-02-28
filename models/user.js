c6d66de4d86be944374c8ddbfdae4d87f3a0d879
AKeyNo <arielaquino.cs@gmail.com>
2021-02-27 19:22:03 -0800

    names to first name and last name

--- models/user.js
+++ models/user.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
     unique: true,
     minlength: 3,
   },
-  name: String,
+  firstName: String,
+  lastName: String,
   passwordHash: String,
   sayings: [
     {
